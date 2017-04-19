26da9581ea270a895103f63be646039d0497d5a9
Kia Fathi <kiafathi@me.com>
2017-04-18 17:57:58 -0700

    (feat) Add basic events

--- events/index.js
+++ events/index.js
@@ -1,1 +1,12 @@
-export default {};
+type EventType = 'presentation_ready' | 'presentation_complete';
+
+const trigger = (event: EventType, payload: Object) => {
+  window.postMessage({
+    event,
+    payload,
+  });
+};
+
+export default {
+  trigger,
+};

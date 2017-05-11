5cb64c61869b563ef00411dfd89aa30bda1e5a4d
Kia Fathi <kiafathi@me.com>
2017-05-11 08:41:57 -0700

    (fix) Remove need for duration in simulator

--- simulator/main.js
+++ simulator/main.js
@@ -6,20 +6,21 @@ var fs = require('fs');
 var runServer = require('./server.js');
 var exec = require('child_process').exec;
 
-
 // MARK: Globals
-program.version('0.0.1')
+program
+  .version('0.0.1')
   .usage('[options]')
   .option('--vars [vars]', 'Application variables', JSON.parse)
   .option('--varfile [varfile]', 'Application variables source file')
   .option('--filesource [source]', 'File source')
   .parse(process.argv);
 
-
 // MARK: Functions
 function fileContents(dir, file, mutator) {
   if (mutator === undefined) {
-    mutator = function(contents) { return contents; };
+    mutator = function(contents) {
+      return contents;
+    };
   }
 
   try {
@@ -36,46 +37,42 @@ function appContents(appDir) {
   return {
     info: fileContents(appDir, 'info.json', JSON.parse),
     strings: fileContents(appDir, 'strings.json', JSON.parse),
-    bundle: !!fileContents(appDir, 'bundle.js') ? appDir + '/bundle.js' : null
+    bundle: !!fileContents(appDir, 'bundle.js') ? appDir + '/bundle.js' : null,
   };
 }
 
 function validateVars(appVars, info) {
-  return info.presentation_properties
-    .concat({name: 'duration', type: 'int'})
-    .every(function(prop) {
-      if (!Object.keys(appVars).includes(prop.name)) {
-        console.log(
-          prop.name + ' not in vars (' + JSON.stringify(appVars) + ')'
-        );
-        return false;
-      }
+  return info.presentation_properties.every(function(prop) {
+    if (!Object.keys(appVars).includes(prop.name)) {
+      console.log(prop.name + ' not in vars (' + JSON.stringify(appVars) + ')');
+      return false;
+    }
 
-      return true;
-    });
+    return true;
+  });
 }
 
 function openChrome() {
   exec(
     '/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome ' +
-    '--auto-open-devtools-for-tabs ' +
-    'http://localhost:3000'
+      '--auto-open-devtools-for-tabs ' +
+      'http://localhost:3000',
   );
 }
-
 
 // MARK: Main
 function main() {
   var app = appContents('.');
 
-  var appVars = program.vars || fileContents('.', program.varfile, JSON.parse) || {};
+  var appVars = program.vars ||
+  fileContents('.', program.varfile, JSON.parse) || {};
   var fileSource = program.filesource || 'http://localhost:3000/static';
 
   if (!program.vars && !program.filesource) {
     console.log(
       'No command-line arguments found. ' +
-      'Ensure that npm is passing arguments through:\n' +
-      'npm run sim -- --vars \'{"duration": 60, ...}\'\n'
+        'Ensure that npm is passing arguments through:\n' +
+        'npm run sim -- --vars \'{"duration": 60, ...}\'\n',
     );
   }
 

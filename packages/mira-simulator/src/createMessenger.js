003b57e19724a5292e25e2d23a77b4aeb2b42d1c
Jordan Schroter <github@jschr.io>
2018-03-29 17:43:54 -0400

    Pre-compile simulator and load app preview in an iframe with separate webpack process.

---
+++ packages/mira-simulator/src/createMessenger.js
@@ -0,0 +1,26 @@
+const source = 'mira-simulator';
+
+// Messenger facilitates communication between the app preview
+// (inner iframe) and the simulator (parent window).
+export default (fromWindow, toWindow, onMessage) => {
+  const receiveMessage = evt => {
+    // Filter out any messages that aren't sent by the simulator or app preview.
+    const isSimulatorMessage =
+      evt.data && typeof evt.data === 'object' && evt.data.source === source;
+
+    if (isSimulatorMessage) {
+      onMessage(evt.data.type, evt.data.payload);
+    }
+  };
+
+  fromWindow.addEventListener('message', receiveMessage, false);
+
+  return {
+    send(type, payload) {
+      toWindow.postMessage({ source, type, payload }, '*');
+    },
+    unlisten() {
+      fromWindow.removeEventListener('message', receiveMessage);
+    },
+  };
+};
---
+++ packages/mira-simulator/src/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Simulator from './Simulator';
+
+ReactDOM.render(<Simulator />, document.getElementById('react-root'));

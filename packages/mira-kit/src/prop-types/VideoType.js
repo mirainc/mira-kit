6d74b853b85551374589f972344f10ac14b86d61
Jordan Schroter <github@jschr.io>
2018-06-28 14:32:47 -0400

    Added webm and m4v to default video content types

--- packages/mira-kit/src/prop-types/VideoType.js
+++ packages/mira-kit/src/prop-types/VideoType.js
@@ -1,7 +1,13 @@
 import FileType from './FileType';
 
 // VideoType is a file type with preset constraints.
-export const videoContentTypes = ['video/mp4', 'video/mpeg', 'video/quicktime'];
+export const videoContentTypes = [
+  'video/mp4',
+  'video/mpeg',
+  'video/quicktime',
+  'video/m4v',
+  'video/webm',
+];
 export default class VideoType extends FileType {
   constructor(label) {
     super(label);

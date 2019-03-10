301ad541d772ed6810f39fd7ee7e17c372a423a3
Leon Li <la2li@uwaterloo.ca>
2019-03-09 19:59:28 -0500

    expose simulator instance on window

--- packages/mira-simulator/src/main.js
+++ packages/mira-simulator/src/main.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Simulator from './Simulator';
 
-ReactDOM.render(<Simulator />, document.getElementById('react-root'));
+// expose simulator instance on window
+const exposeSimulator = simulator => {
+  window.simulator = simulator;
+};
+
+ReactDOM.render(
+  <Simulator ref={exposeSimulator} />,
+  document.getElementById('react-root'),
+);

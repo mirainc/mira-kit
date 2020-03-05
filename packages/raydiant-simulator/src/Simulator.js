cc9bf361685502e856e34551197a7e4adacb57d0
Sebastian Key <sebastianwkey@gmail.com>
2020-03-04 17:58:33 -0800

    Remove unused var

--- packages/raydiant-simulator/src/Simulator.js
+++ packages/raydiant-simulator/src/Simulator.js
@@ -16,7 +16,6 @@ import mergeDefaultAppVars from './mergeDefaultAppVars';
 
 const PRESENTATION_MIN_DURATION = 5;
 const EMPTY_PRESENTATION = { name: 'New Presentation', application_vars: {} };
-const EMPTY_APP_VERSION = { icon_url: '', presentation_properties: [] };
 
 class RaydiantAppSimulator extends Component {
   initialState = {
@@ -314,8 +313,8 @@ class RaydiantAppSimulator extends Component {
     presentation = presentation || EMPTY_PRESENTATION;
 
     if (!appVersion) {
-      // rendering the preview also indirectly updates this.state.appVersion
-      // using the values defined in raydiant.config.js
+      // rendering the preview also indirectly updates
+      // this.state.appVersion using the values defined in raydiant.config.js
       return this.renderPreview(presentation, [], previewMode);
     }
 

afce593030d83296607de3f0596f2621447db280
Sebastian Key <sebastianwkey@gmail.com>
2020-04-07 09:52:06 -0700

    Favour “label” over “name”

--- packages/raydiant-kit/src/prop-types/extractProperties.ts
+++ packages/raydiant-kit/src/prop-types/extractProperties.ts
@@ -64,7 +64,7 @@ export default function extractProperties(
       (propType as RadioSelectionPropType).options.forEach(option => {
         strings[option.value] = option.label;
         prop.options.push({
-          name: option.value,
+          label: option.label,
           value: option.value,
           iconName: option.iconName,
         });

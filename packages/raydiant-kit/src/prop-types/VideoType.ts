fd27669f7094b706d1cb0c557f16323fe434e374
VinhV + BaoH <vinh.vu@eastagile.com>
2020-08-13 11:29:43 +0700

    Revert "[b] - fix VideoType accept types to work with FireFox (#138)"
    Move this fix to raydiant-elements
    This reverts commit 437170f98530f5790a1b865afcbc32b5a9ee83fc.

--- packages/raydiant-kit/src/prop-types/VideoType.ts
+++ packages/raydiant-kit/src/prop-types/VideoType.ts
@@ -5,8 +5,8 @@ export const videoContentTypes = [
   'video/mp4',
   'video/mpeg',
   'video/quicktime',
-  'video/x-m4v',
-  '.webm',
+  'video/m4v',
+  'video/webm',
 ];
 export default class VideoType extends FileType {
   constructor(label: string) {
